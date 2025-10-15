b4e9451a2719648e05ae10dd990f6d02dabf0c02
Maria Maksimova <drogzed@gmail.com>
2025-10-15 22:21:53 +0200

    test

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char myChar = 99;   // 'c'
+        char myCharTwo = 104; // 'h'
+        char myCharThree = 97; // 'a'
+        char myCharFour = 114; // 'r'
+        System.out.println(myChar);
+        System.out.println(myCharTwo);
+        System.out.println(myCharThree);
+        System.out.println(myCharFour);
     }
+
 }
